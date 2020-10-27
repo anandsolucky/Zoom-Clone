7496b69ff930425fc68681316ace03f2d647205b
unknown <anand.solucky@gmail.com>
2020-10-27 13:09:02 +0530

    bootstrap added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname,'../templates/views');
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
+app.set('views', viewsPath)
+app.use(express.static(publicPath))
 
+app.get('/', (req, res)=> {
+    res.render('index')
+})
 
 app.listen(PORT, () => {
     console.log("listing on the port " + PORT);
