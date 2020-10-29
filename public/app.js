0823a945a746b19818e6c7a7e260ae0d1c4370f3
unknown <anand.solucky@gmail.com>
2020-10-29 18:12:08 +0530

    added heroku port for peer

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  port: '443'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
