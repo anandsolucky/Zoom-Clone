41955fb619a940a3a6e90d348d20fe8e835255c6
unknown <anand.solucky@gmail.com>
2020-10-29 17:30:15 +0530

    heroku commit, added 441 prt

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
   host: '/',
-  port: '3001'
+  port: '441'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
